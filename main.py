0f268e0013e52c2db3c9dcb80d86e5506b7dcd06
DESKTOP-3S7C650\ELLE <ellestpd@hotmail.com>
2021-03-24 07:53:38 +0700

    Dash board - User Show

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
+import os.path
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5 import QtWidgets
-
+from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
 from dataextraction.GetEmployeeInfo import getEmployeeInfo
 from dataextraction.GetDashboardInfo import *
 from App_UI_LOG import Ui_MainWindow
 from ApiModule.TimeAttendaceAPI import *
-import os.path
+
 
 class MainWindow:
     def __init__(self):
-
+        
         if os.path.isfile('URL.txt'):
             f = open("URL.txt", "r")
 
@@ -36,10 +39,27 @@ class MainWindow:
         self.Passwd = str(self.ui.lineEdit_Password.text())
 
         global df_person_input , df_record_input
-        df_record_input = access_record('01/01/2020', datetime.today().strftime("%d/%m/%Y"),self.url,self.User,self.Passwd)
-        df_person_input = get_persons(self.url,self.User,self.Passwd)
-        datetime_input = datetime.today().strftime("%Y/%m/%d")
+        
+        # df_record_input = access_record('01/01/2020', datetime.today().strftime("%d/%m/%Y"),self.url,self.User,self.Passwd)
+        # df_person_input = get_persons(self.url,self.User,self.Passwd)
+        # datetime_input = datetime.today().strftime("%Y/%m/%d")
+        # self.TimeShow , self.user = daily_scan(df_record_input, df_person_input, datetime_input)
+
+        # Test by CSV
+        datetime_input = '2021/01/15'
+        df_record_input = pd.read_csv('exportAceesRecord2.csv')
+        df_person_input = pd.read_csv('exportGetPerson.csv')
         self.TimeShow , self.user = daily_scan(df_record_input, df_person_input, datetime_input)
+        print(self.user)
+
+
+        # Show User
+        self.ui.lcdNumber_Employee.setProperty("value", self.user['Employee'])
+        self.ui.lcdNumber_Visito.setProperty("value", self.user['Visitor'])
+        self.ui.lcdNumber_Blacklist.setProperty("value", self.user['Blacklist'])
+        self.All = self.user['Employee']+self.user['Visitor']+self.user['Blacklist']
+        self.ui.lcdNumber_ALL.setProperty("value", self.All)
+
 
         self.ui.stackedWidget.setCurrentWidget(self.ui.Login)
         self.ui.pushButton_Dashbord_dash.clicked.connect(self.Dashbord)
@@ -59,18 +79,20 @@ class MainWindow:
     def Dashbord(self):
         if self.P == 1:
             self.ui.stackedWidget.setCurrentWidget(self.ui.Dashbord_page)
-    def Employee(self,df):
+
+
+    def Employee(self):
         if self.P == 1:
             self.ui.stackedWidget.setCurrentWidget(self.ui.Employee_page)
 
-        """
-            Get information in dataframe and add to QTableWidgets
-            
-            :Param
-            df (pandas.core.frame.DataFrame) : Exployee dataframe   
-            
-        """
-            em_df = getEmployeeInfo(df)
+            """
+                Get information in dataframe and add to QTableWidgets
+                
+                :Param
+                df (pandas.core.frame.DataFrame) : Exployee dataframe   
+                
+            """
+            em_df = getEmployeeInfo(df_person_input)
 
             nRows = len(em_df.index)
             nColumns = len(em_df.columns)
@@ -91,14 +113,14 @@ class MainWindow:
     def log_in(self):
         self.User = str(self.ui.lineEdit_Username.text())
         self.Passwd = str(self.ui.lineEdit_Password.text())
-        # self.Log_Run()
-        self.result_login = TimeAttendaceAPI.get_seria_no(self.url,self.User,self.Passwd)
+        self.Log_Run()
+        # self.result_login = TimeAttendaceAPI.get_seria_no(self.url,self.User,self.Passwd)
 
 
-        if self.result_login == True:
-            self.Log_Run()
-        else:
-            self.P =0
+        # if self.result_login == True:
+        #     self.Log_Run()
+        # else:
+        #     self.P =0
 
     def Add_Pad(self):
         self.url_input = str(self.ui.lineEdit_Addpad.text())
