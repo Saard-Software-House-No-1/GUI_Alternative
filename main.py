55c77dd74f0248fd9ddd877c27f0d17db402df9e
pattaraporn punya <pattaraporn.fibo@mail.kmutt.ac.th>
2021-03-30 01:17:25 +0700

    prepare args already and call report module but still have bug when cannot find enployee name in record df

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from PyQt5 import QtWidgets
 from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
+import datetime
 
 from dataextraction.GetEmployeeInfo import getEmployeeInfo
 from dataextraction.GetDashboardInfo import *
 
+from generate_csv.generate_csv import generate_csv
+
 from App_UI_LOG import Ui_MainWindow
 # from ApiModule.TimeAttendaceAPI import *
 
+global config_dict 
+config_dict = {'shift_start':(9,0),'shift_end':(18,0)}
 
 class MainWindow:
     def __init__(self):
@@ -48,19 +53,19 @@ class MainWindow:
         # self.TimeShow , self.user = daily_scan(df_record_input, df_person_input, datetime_input)
 
         # Test by CSV
-        datetime_input = '2021/01/15'
+        # datetime_input = '2021/01/15'
         df_record_input = pd.read_csv('exportAceesRecord2.csv')
         df_person_input = pd.read_csv('exportGetPerson.csv')
-        self.TimeShow , self.user = daily_scan(df_record_input, df_person_input, datetime_input)
-        print(self.user)
+        # self.TimeShow , self.user = daily_scan(df_record_input, df_person_input, datetime_input)
+        # print(self.user)
 
 
-        # Show User
-        self.ui.lcdNumber_Employee.setProperty("value", self.user['Employee'])
-        self.ui.lcdNumber_Visito.setProperty("value", self.user['Visitor'])
-        self.ui.lcdNumber_Blacklist.setProperty("value", self.user['Blacklist'])
-        self.All = self.user['Employee']+self.user['Visitor']+self.user['Blacklist']
-        self.ui.lcdNumber_ALL.setProperty("value", self.All)
+        # # Show User
+        # self.ui.lcdNumber_Employee.setProperty("value", self.user['Employee'])
+        # self.ui.lcdNumber_Visito.setProperty("value", self.user['Visitor'])
+        # self.ui.lcdNumber_Blacklist.setProperty("value", self.user['Blacklist'])
+        # self.All = self.user['Employee']+self.user['Visitor']+self.user['Blacklist']
+        # self.ui.lcdNumber_ALL.setProperty("value", self.All)
 
 
         self.ui.stackedWidget.setCurrentWidget(self.ui.Login)
@@ -71,14 +76,15 @@ class MainWindow:
         self.ui.pushButton_Login.clicked.connect(self.log_in)
         self.ui.pushButton_Addpad.clicked.connect(self.Add_Pad)
         self.ui.pushButton_CSV.clicked.connect(self.export_csv)
+        self.ui.pushButton_Setshift_2.clicked.connect(self.setShift)
 
 
     def show(self):
         self.main_win.show()
         self.s = '\n'
         self.url_show = self.s.join(self.url)
-        self.ui.lineEdit_Addpad_show.setText(self.url_show)
-        print(self.url)
+        # self.ui.lineEdit_Addpad_show.setText(self.url_show)
+        # print(self.url)
 
     def Dashbord(self):
         if self.P == 1:
@@ -111,6 +117,7 @@ class MainWindow:
     def Config(self):
         if self.P == 1:
             self.ui.stackedWidget.setCurrentWidget(self.ui.Config_page)
+
     def Report(self):
         if self.P == 1:
 
@@ -125,7 +132,7 @@ class MainWindow:
                 self.listEmployee[i] = QtWidgets.QCheckBox(self.ui.scrollAreaWidgetContents)
                 self.ui.verticalLayout_2.addWidget(self.listEmployee[i])
                 self.listEmployee[i].setText(em_list[i])
-            
+  
             
     def log_in(self):
         self.User = str(self.ui.lineEdit_Username.text())
@@ -158,7 +165,7 @@ class MainWindow:
         self.P=1
         
     def create_piechart(self):
-        print(self.user)
+        # print(self.user)
         series = QPieSeries()
         series.append("Ontime", self.TimeShow['Ontime'])
         series.append("Late", self.TimeShow['Late'])
@@ -202,7 +209,49 @@ class MainWindow:
             if(i.isChecked()):
                 csv_checklist.append(i.text())
 
+        start_date = self.ui.dateEdit_start.text().split('/')
+        start_date_dt = datetime(year=int(start_date[2]), month=int(start_date[0]), day=int(start_date[1]))
+        end_date = self.ui.dateEdit_End.text().split('/')
+        end_date_dt = datetime(year=int(end_date[2]), month=int(end_date[0]), day=int(end_date[1]))
 
+        print('csv_checklist = '+str(csv_checklist))
+        if (self.ui.radioButton_personal.isChecked() and self.ui.radioButton_OT.isChecked()):
+            print('personal and ot')
+            generate_csv(df_record_input,df_record_input,start_date_dt,end_date_dt,'OT',config_dict,csv_checklist,'D:\TimeAttendence')
+        elif (self.ui.radioButton_personal.isChecked() and self.ui.radioButton_Timeattendance.isChecked()):
+            print('personal and time attendance')
+            generate_csv(df_record_input,df_record_input,start_date_dt,end_date_dt,'Time attendance',config_dict,csv_checklist,'D:\TimeAttendence')
+        elif (self.ui.radioButton_Allemployee.isChecked() and self.ui.radioButton_OT.isChecked()):
+            print('all employee and ot')
+            generate_csv(df_record_input,df_record_input,start_date_dt,end_date_dt,'OT',config_dict,self.listEmployee,'D:\TimeAttendence')
+        elif(self.ui.radioButton_Allemployee.isChecked() and self.ui.radioButton_Timeattendance.isChecked()):
+            print('all employee and time attendance')
+            generate_csv(df_record_input,df_record_input,start_date_dt,end_date_dt,'Time attendance',config_dict,self.listEmployee,'D:\TimeAttendence')
+
+
+    def setShift(self):
+        
+        shift_start = self.ui.timeEdit_start_config_2.text()
+        shift_start_hour = int(shift_start.split(':')[0])
+        shift_start_min = int(shift_start.split(':')[1].split(' ')[0])
+        am_pm_start = shift_start.split(':')[1].split(' ')[1]
+
+        if am_pm_start =='PM' and shift_start_hour < 12:
+            shift_start_hour += 12
+        elif am_pm_start =='AM' and shift_start_hour == 12:
+            shift_start_hour = 0
+
+        shift_end = self.ui.timeEdit_End_config_2.text()
+        shift_end_hour = int(shift_end.split(':')[0])
+        shift_end_min = int(shift_end.split(':')[1].split(' ')[0])
+        am_pm_end = shift_end.split(':')[1].split(' ')[1]
+
+        if am_pm_end =='PM' and shift_end_hour < 12:
+           shift_end_hour += 12
+        elif am_pm_end =='AM' and shift_end_hour == 12:
+           shift_end_hour = 0
+            
+        config_dict = {'shift_start':(shift_start_hour,shift_start_min),'shift_end':(shift_end_hour,shift_end_min)}
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
